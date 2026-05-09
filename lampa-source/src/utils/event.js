4ee3939c561f4d6e544f918d909aa1da318dfb04
tri3gubki <tri3gubki@gmail.com>
2026-05-09 12:20:52 +0200

    stub: core/socket.js + utils/event.js → no-op (CUB-Socket удалён, имитируем API чтобы импорты в storage/workers.js, full/start*.js, account/* резолвились)

--- lampa-source/src/utils/event.js
+++ lampa-source/src/utils/event.js
@@ -1,49 +1,11 @@
-import Utils from './utils'
-import Socket from '../core/socket'
+// События через сокет — CUB-Socket удалён, класс превращён в no-op,
+// чтобы существующие `this.event.call(...) / cancel(...) / destroy()` в
+// full/start*.js не падали. Никаких реальных колбэков от сервера нет.
 
-/**
- * События через сокет
- * @returns {void}
- */
 function Event(){
-    let ids    = {}
-    let evokes = {}
-
-    function callback(data){
-        if(data.method == 'callback' && ids[data.callback_name] == data.callback_id){
-            evokes[data.callback_id](data)
-
-            evokes[data.callback_id] = ()=>{}
-        }
-    }
-
-    this.call = function(method, params, call){
-        if(!ids[method]) ids[method] = Utils.uid(10)
-
-        params.callback_id   = ids[method]
-        params.callback_name = method
-
-        evokes[params.callback_id] = call
-
-        Socket.send('callback',params)
-    }
-
-    this.cancel = function(method){
-        if(ids[method]){
-            evokes[ids[method]] = ()=>{}
-        }
-    }
-
-    this.destroy = function(){
-        Socket.listener.remove('message', callback)
-
-        for(let i in evokes){
-            evokes[i] = ()=>{}
-        }
-    }
-
-    Socket.listener.follow('message', callback)
+    this.call    = function(){}
+    this.cancel  = function(){}
+    this.destroy = function(){}
 }
-
 
 export default Event
